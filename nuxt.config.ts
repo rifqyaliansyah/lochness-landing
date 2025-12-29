d05df095d51131ee8a6922b79b32922382a1b93a
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2025-12-29 12:47:21 +0700

    add favicon and change name for tab

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: 'Lochness Terminal',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Lochness Terminal' }
+      ],
+      // link: [
+      //   { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      // ]
+    }
+  }
 })
